c7bdf74f0877a2d571e8f1e79a0aa5464bc82cb3
Aman <amanpatel96917@gmail.com>
2023-09-03 15:38:20 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,54 @@
+import React, { useState } from 'react';
+import './App.css';
+
+function App() {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    
+    const formData = { name, email };
+
+    try {
+      const response = await fetch('http://localhost:3001/Person', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/x-www-form-urlencoded',
+        },
+        body: new URLSearchParams(formData)
+      });
+
+      if(response.ok) {
+        const result = await response.json();
+        console.log(result);
+        alert('Data saved successfully');
+      } else {
+        throw new Error('Failed to save data');
+      }
+    } catch (error) {
+      console.error("There was an error saving the data:", error);
+    }
+  };
+
+  return (
+    <div className="App">
+      <h2>Submit Form</h2>
+      <form onSubmit={handleSubmit}>
+        <label>
+          Name:
+          <input type="text" value={name} onChange={e => setName(e.target.value)} />
+        </label>
+        <br />
+        <label>
+          Email:
+          <input type="email" value={email} onChange={e => setEmail(e.target.value)} />
+        </label>
+        <br />
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  );
+}
+
+export default App;
